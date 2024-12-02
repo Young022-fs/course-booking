b97cd65caac70d4773a4418a4ce58f65195316f1
Young Neboh <davidsonneb@gmail.com>
2024-12-02 17:54:05 +0400

    Cleaned up some files

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ app.use(express.json());
 app.set('json spaces', 3);
 const path = require('path');
 let PropertiesReader = require("properties-reader");
-// Load properties from the file
+
 let propertiesPath = path.resolve(__dirname, "./dbconnection.properties");
 let properties = PropertiesReader(propertiesPath);
 
-// Extract values from the properties file
+
 const dbPrefix = properties.get('db.prefix');
 const dbHost = properties.get('db.host');
 const dbName = properties.get('db.name');
@@ -19,12 +19,12 @@ const dbPassword = properties.get('db.password');
 const dbParams = properties.get('db.params');
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// MongoDB connection URL
+
 const uri = `${dbPrefix}${dbUser}:${dbPassword}${dbHost}${dbParams}`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
 
-let db1;//declare variable
+let db1;
 
 async function connectDB() {
   try {
@@ -36,24 +36,24 @@ async function connectDB() {
   }
 }
 
-connectDB(); //call the connectDB function to connect to MongoDB database
+connectDB();
 
-//Optional if you want the get the collection name from the Fetch API in test3.html then
+
 app.param('collectionName', async function(req, res, next, collectionName) { 
     req.collection = db1.collection(collectionName);
-    /*Check the collection name for debugging if error */
+    
     console.log('Middleware set collection:', req.collection.collectionName);
     next();
 });
 
 app.get('/collections/:collectionName', async function(req, res, next) {
-    console.log("Fetching data from collection:", req.collection.collectionName);  // Log the collection name
+    console.log("Fetching data from collection:", req.collection.collectionName); 
     try {
         const results = await req.collection.find({}).toArray();
         if (results.length === 0) {
             return res.status(404).json({ message: 'No data found' });
         }
-        console.log("Results fetched:", results);  // Log the fetched results
+        console.log("Results fetched:", results); 
         res.json(results);
     } catch (err) {
         console.error('Error fetching docs', err.message);
@@ -81,8 +81,8 @@ app.get('/collections1/:collectionName', async function(req, res, next) {
 app.get('/collections/:collectionName/:max/:sortAspect/:sortAscDesc', async function(req, res, next){
   try{
 
-    // TODO: Validate params
-var max = parseInt(req.params.max, 10); // base 10
+
+var max = parseInt(req.params.max, 10);
 let sortDirection = 1;
 if (req.params.sortAscDesc === "desc") {
  sortDirection = -1;
@@ -173,7 +173,6 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: 'An error occurred' });
 });
 
-// Start the server
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
   });
